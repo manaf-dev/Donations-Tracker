8341b02443cb9291966437ef9858de75345b94b7
rems <tech1.rems@gmail.com>
2024-05-02 15:59:35 +0000

    Migrated the created models, Designed the home interface with html and css.

--- DonationTracker/settings.py
+++ DonationTracker/settings.py
@@ -127,6 +127,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static'
+# STATICFILES_DIRS = [BASE_DIR / 'donateApp/static']
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
--- donateApp/views.py
+++ donateApp/views.py
@@ -4,4 +4,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('Are you willing to donate?')
+    return render(request, 'donateApp/index.html')
+
+
+def events(request):
+    return render(request, 'donateApp/events.html')
